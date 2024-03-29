e198f49209c835ec6850e50b554aa5608d4d2501
Omartarek78 <omartarek78@gmail.com>
2024-03-29 07:49:25 +0200

    all drivers added

---
+++ CONTROL_ECU/buzzer.h
@@ -0,0 +1,19 @@
+/*
+ * buzzer.h
+ *
+ *  Created on: Mar 29, 2024
+ *      Author: Omar Tarek
+ */
+
+#ifndef BUZZER_H_
+#define BUZZER_H_
+#include"std_types.h"
+#include "common_macros.h"
+#include "gpio.h"
+#define BUZZER_PORT PORTD_ID
+#define BUZZER_PIN PIN6_ID
+void Buzzer_init(void);
+void Buzzer_on(void);
+void Buzzer_off(void);
+
+#endif /* BUZZER_H_ */
