691a69ac6732d27f64a20224474b6e32ad2a4404
Omartarek78 <omartarek78@gmail.com>
2024-04-29 17:01:00 +0300

    final changes

---
+++ WS/Door_locker_security_system_control_ECU/Debug/control_ecu.d
@@ -0,0 +1,25 @@
+control_ecu.o control_ecu.o: ../control_ecu.c ../control_ecu.h ../twi.h \
+  ../std_types.h ../uart.h ../external_eeprom.h ../buzzer.h \
+  ../common_macros.h ../gpio.h ../dc_motor.h ../pwm.h ../timer1.h
+
+../control_ecu.h:
+
+../twi.h:
+
+../std_types.h:
+
+../uart.h:
+
+../external_eeprom.h:
+
+../buzzer.h:
+
+../common_macros.h:
+
+../gpio.h:
+
+../dc_motor.h:
+
+../pwm.h:
+
+../timer1.h:
